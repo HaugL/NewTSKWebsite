b8f31d9a0f2365dcdbdf3ae1405b400ed907e9e8
HaugL <lianne.haug@colorado.edu>
2014-02-05 19:29:04 -0700

    removed sources from project

--- app/models/source.rb
+++
@@ -1,9 +0,0 @@
-class Source < ActiveRecord::Base
-	has_many :products
-  	
-  	attr_accessible :name, :location, :description
-
-  	  validates :name, :presence => :true
-  	  validates :location, :presence => :true
-  	  validates :description, :presence => :true
-end
